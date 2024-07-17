7e46cc6ef5e0c9f23f46e2726dddf4e6a7ad8747
Abuga Aroni <abugaaroni@gmail.com>
2024-07-17 16:38:08 +0300

    added jquery

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//user interface logic
+$(document).ready(function() {
+
+//when the my cv button is clicked, the show cv div should be shown
+$('#cv').click(function(event) {
+    event.preventDefault();
+    $
+})
+
+
+}
