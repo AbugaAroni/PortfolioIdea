406b71b39185e3338e21c1cd905b64c939294ca8
Abuga Aroni <abugaaroni@gmail.com>
2024-07-17 16:54:53 +0300

    added simple javascript for viewing a cv

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,19 @@
 $(document).ready(function() {
 
 //when the my cv button is clicked, the show cv div should be shown
-$('#cv').click(function(event) {
+$("#cv").click(function(event) {
     event.preventDefault();
-    $
-})
+    $("#Abugacv").removeClass('hide-div');
+    $("#backimg").addClass('bgview');   
+    $("#backimg").removeClass('bghome');
+});
 
+//when the my hidecv button is clicked, the cv should hbe hidden
+$("#hidecv").click(function(event) {
+    event.preventDefault();
+    $("#Abugacv").addClass('hide-div');
+    $("#backimg").addClass('bghome');   
+    $("#backimg").removeClass('bgview');
+});
 
-}
+});
